ce3bced2d8771b21c6439bfaf67691057051839d
dianaavalos <diana.avalos.m@gmail.com>
2019-11-21 15:24:18 +0100

    added samples randomization

--- diana_scripts/build_metabamfile_v3.py
+++ diana_scripts/build_metabamfile_v3.py
@@ -2,6 +2,7 @@ import os
 import glob
 import subprocess
 import sys
+import random
 
 datafolder = sys.argv[1]
 outputfile = sys.argv[2]
@@ -10,17 +11,21 @@ outputtag = outputfile.replace('.bam','')
 #print(datafolder)
 #print(outputfile)
 
-i = 1
-for filename in glob.iglob(datafolder+'/*.bam'):
-	subsample = "/data/unige/funpopgen/odelanea/SHARE/downsampleBAM/bin/sampleBAM "+filename+" 1000000 "+outputtag+"_"+str(i)+".bam"
-	print(subsample)
-	os.system(subsample)
-	i = i+1
-	if(i>50):
-		break
+# random selection of samples indices, making sure no sample is picked twice
+number_of_samples = 50
+files = glob.glob(datafolder+'/*.bam')
+samples_selected = list(range(number_of_samples))
+random.shuffle(samples_selected)
 
-mergebam = '/software/UHTS/Analysis/samtools/1.4/bin/samtools merge '+outputfile+' '+outputtag+"_*.bam"
+
+for i in range(len(files)):
+    if i in samples_selected:
+        # glob.glob cannot find recursively in subfolder but needs the exact folder. otherwise: https://stackoverflow.com/questions/2186525/how-to-use-glob-to-find-files-recursively
+        subsample = "/data/unige/funpopgen/odelanea/SHARE/downsampleBAM/bin/sampleBAM " + files[i] + " 1000000 " + outputtag + "_" + str(i) + ".bam"
+        print(subsample)
+        os.system(subsample)
+
+
+mergebam = '/software/UHTS/Analysis/samtools/1.4/bin/samtools merge ' + outputfile + ' ' + outputtag + "_*.bam"
 print(mergebam)
 os.system(mergebam)
-
-
