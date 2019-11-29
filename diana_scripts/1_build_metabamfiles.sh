2c640af834ed43f42b469177ccb6fb17c74abe04
Diana Avalos <diana.avalos.m@gmail.com>
2019-11-29 13:35:42 +0100

    added samples to exclude in the script

--- diana_scripts/1_build_metabamfiles.sh
+++ diana_scripts/1_build_metabamfiles.sh
@@ -7,6 +7,7 @@ declare -a Histone_Modifications=("H3K4me1" "H3K27ac")
 # folders
 DATA_FOLDER=/data/unige/funpopgen/2backup/grey2/external_data/Blueprint_immune_cells
 OUTPUT_FOLDER=/data/unige/funpopgen/davalos/project/IMMUNE_CELLS/step1_metabam
+SAMPLES_EXCLUDED=/data/unige/funpopgen/davalos/project/IMMUNE_CELLS/diana_scripts/samples_to_exclude.txt
 PY_SCRIPT=/data/unige/funpopgen/davalos/project/IMMUNE_CELLS/diana_scripts/build_metabamfile_v3.py
 
 mkdir -p $OUTPUT_FOLDER
@@ -15,7 +16,7 @@ mkdir -p $OUTPUT_FOLDER
 for cell_type in ${Cell_Types[@]}; do
     for histone in ${Histone_Modifications[@]}; do
         echo "$cell_type""_$histone"
-        cmd="bsub -o $OUTPUT_FOLDER/buildmetabamfile_$cell_type""_$histone.out -e $OUTPUT_FOLDER/buildmetabamfile_$cell_type""_$histone.err -J buildmetabamfile_$cell_type""_$histone -q normal -M 64000000 \"python3.5 $PY_SCRIPT $DATA_FOLDER/$cell_type/$histone $OUTPUT_FOLDER/$cell_type""_$histone""_50samples.metafile.bam\""
+        cmd="bsub -o $OUTPUT_FOLDER/buildmetabamfile_$cell_type""_$histone.out -e $OUTPUT_FOLDER/buildmetabamfile_$cell_type""_$histone.err -J buildmetabamfile_$cell_type""_$histone -q normal -M 64000000 \"python3.5 $PY_SCRIPT $DATA_FOLDER/$cell_type/$histone $OUTPUT_FOLDER/$cell_type""_$histone""_50samples.metafile.bam\" $SAMPLES_EXCLUDED"
         echo "$cmd"
         eval $cmd
         #$cmd does not work because somehow it prints the \ in EGAD00001002670_H3K27ac_50samples.metafile.bam\"
@@ -23,4 +24,3 @@ for cell_type in ${Cell_Types[@]}; do
 done
 
 
-# btw 30min and 2h to complete jobs
