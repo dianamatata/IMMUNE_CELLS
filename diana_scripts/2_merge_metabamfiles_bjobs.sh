776b81d4b6751cc42d4e14f4a58750236f0dbaee
Diana Avalos <diana.avalos.m@gmail.com>
2019-12-02 14:25:00 +0100

    not tested yet new way of running it

--- diana_scripts/2_merge_metabamfiles_bjobs.sh
+++ diana_scripts/2_merge_metabamfiles_bjobs.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
 
 # merge metabamfiles with samtools
-cmd="samtools merge H3K4me1.metafile.bam EGAD0000100267?_H3K4me1_50samples.metafile.bam"
-bsub -o samtools_merging_H3K4me1.out -R "rusage[mem=20]" -M 20000 -n 30 -R "span[hosts=1]"  -J "samtools_merging_H3K4me1" " $cmd"
+cmd="samtools merge H3K4me1.metafile.bam EGAD0000100267?_H3K4me1_50samples.metafile.bam
+samtools sort -o H3K4me1.sorted.metafile.bam H3K4me1.metafile.bam"
+bsub -o samtools_merging_and_sorting_H3K4me1.out -R "rusage[mem=200]" -M 200000 -n 30 -R "span[hosts=1]" -J "samtools_merging_and_sorting_H3K4me1" " $cmd"
 echo "H3K4me1.metafile.bam created"
 
-cmd="samtools merge H3K27ac.metafile.bam EGAD0000100267?_H3K27ac_50samples.metafile.bam"
-bsub -o samtools_merging_H3K27ac.out -R "rusage[mem=20]" -M 20000 -n 30 -R "span[hosts=1]"  -J "samtools_merging_H3K27ac" " $cmd"
+
+cmd="samtools merge H3K27ac.metafile.bam EGAD0000100267?_H3K27ac_50samples.metafile.bam
+samtools sort -o H3K27ac.sorted.metafile.bam H3K27ac.metafile.bam"
+bsub -o samtools_merging_and_sorting_H3K27ac.out -R "rusage[mem=200]" -M 200000 -n 30 -R "span[hosts=1]" -J "samtools_merging_and_sorting_H3K27ac" " $cmd"
 echo "H3K27ac.metafile.bam created"
 
-# cannot launch the next phase while the previous part is not done otherwise job fails
-# maybe join 2 cmd of same histone in only one cmd?
-
-# sort metabam files
-cmd="samtools sort -o H3K27ac.sorted.metafile.bam H3K27ac.metafile.bam"
-bsub -o samtools_sorting_H3K27ac.out -R "rusage[mem=200]" -M 200000 -n 30 -R "span[hosts=1]"  -J "samtools_sorting_H3K27ac" " $cmd"
-echo "H3K27ac.sorted.metafile.bam created"
-
-cmd="samtools sort -o H3K4me1.sorted.metafile.bam H3K4me1.metafile.bam"
-bsub -o samtools_sorting_H3K4me1.out -R "rusage[mem=200]" -M 200000 -n 30 -R "span[hosts=1]"  -J "samtools_sorting_H3K4me1" " $cmd"
-echo "H3K4me1.sorted.metafile.bam created"
 
 
