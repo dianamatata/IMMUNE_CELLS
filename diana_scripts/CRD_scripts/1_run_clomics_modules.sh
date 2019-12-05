9465b64ec2c77cada26a2e951342208ae04c6484
Diana Avalos <diana.avalos.m@gmail.com>
2019-12-05 10:31:19 +0100

    remioved a + error in echoing

--- diana_scripts/CRD_scripts/1_run_clomics_modules.sh
+++ diana_scripts/CRD_scripts/1_run_clomics_modules.sh
@@ -12,14 +12,13 @@ declare -a Cell_Types=("EGAD00001002673" "EGAD00001002672" "EGAD00001002670")
 LI=$DATAFOLDER/merged_residuals.bed.gz
 
 for cell_type in ${Cell_Types[@]}; do
-  CELL_TYPE="$cell_type"+"_ALL"
+    CELL_TYPE="$cell_type""_ALL"
 
-  for c in $(seq 1 22); do
-          LO=$DATAFOLDER/$CELL_TYPE\.chr$c
-          echo "$LO"
-          echo"$DATAFOLDER/EGAD00001002670_ALL\.chr$c"
-          #echo "$CLOMICs build --bed $LI --region $c --out $LO\.1.tgz --silent && $CLOMICs topo --bed $LI --tree $LO\.1.tgz --chr $c --out $LO\.tree.txt.gz && rm $LO\.*.tgz" | bsub -o $LO\.log -R "rusage[mem=8000]" -M 8000000
-  done
+    for c in $(seq 1); do
+        LO=$DATAFOLDER/$CELL_TYPE\.chr$c
+        echo "$LO"
+        echo "$CLOMICs build --bed $LI --region $c --out $LO\.1.tgz --silent && $CLOMICs topo --bed $LI --tree $LO\.1.tgz --chr $c --out $LO\.tree.txt.gz && rm $LO\.*.tgz" | bsub -o $LO\.log -R "rusage[mem=8000]" -M 8000000
+    done
 done
 
 #  #exit
@@ -37,7 +36,7 @@ done
 #          zcat $LO\.txt.gz | awk '{ if ($30 == 1 && $25 > 1) print $4 }'
 #  done | gzip -c > $DATAFOLDER/EGAD00001002670_ALL.modules.MOD1.NRE2.txt.gz
 
-done
+#done
 
 
 
