8566b5f147a154372c45e1941ac9199ae97ec173
Diana Avalos <diana.avalos.m@gmail.com>
2019-12-05 10:31:42 +0100

    removed temp files

--- diana_scripts/2_merge_metabamfiles.sh
+++ diana_scripts/2_merge_metabamfiles.sh
@@ -7,6 +7,6 @@ samtools merge H3K27ac.metafile.bam EGAD0000100267?_H3K27ac_50samples.metafile.b
 echo "H3K27ac.metafile.bam created"
 
 # sort metabam files
-samtools sort -T H3K27ac_temp -o H3K27ac.sorted.metafile.bam H3K27ac.metafile.bam      
-samtools sort -T H3K4me1_temp -o H3K4me1.sorted.metafile.bam H3K4me1.metafile.bam  
+samtools sort -o H3K27ac.sorted.metafile.bam H3K27ac.metafile.bam      
+samtools sort -o H3K4me1.sorted.metafile.bam H3K4me1.metafile.bam  
 echo "H3K27ac.sorted.metafile.bam and H3K4me1.sorted.metafile.bam created: H3K4me1.metafile.bam and H3K27ac.metafile.bam sorted"
