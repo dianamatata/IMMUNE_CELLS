e4dda7f9667f5c53c104eb4e4d55a950baeae1ae
Diana Avalos <diana.avalos.m@gmail.com>
2019-12-05 10:32:27 +0100

    && for sequencial run of commands otherwise errors

--- diana_scripts/2_merge_metabamfiles_bjobs.sh
+++ diana_scripts/2_merge_metabamfiles_bjobs.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
 
 # merge metabamfiles with samtools
-cmd="samtools merge H3K4me1.metafile.bam EGAD0000100267?_H3K4me1_50samples.metafile.bam
-samtools sort -o H3K4me1.sorted.metafile.bam H3K4me1.metafile.bam"
-bsub -o samtools_merging_and_sorting_H3K4me1.out -R "rusage[mem=200]" -M 200000 -n 30 -R "span[hosts=1]" -J "samtools_merging_and_sorting_H3K4me1" " $cmd"
+cmd="samtools merge H3K4me1.metafile.bam EGAD0000100267?_H3K4me1_50samples.metafile.bam && samtools sort -o H3K4me1.sorted.metafile.bam H3K4me1.metafile.bam"
+bsub -o samtools_merging_and_sorting_H3K4me1.out -R "rusage[mem=2000]" -M 2000000 -n 30 -R "span[hosts=1]" -J "samtools_merging_and_sorting_H3K4me1" " $cmd"
 echo "H3K4me1.metafile.bam created"
 
 
-cmd="samtools merge H3K27ac.metafile.bam EGAD0000100267?_H3K27ac_50samples.metafile.bam
-samtools sort -o H3K27ac.sorted.metafile.bam H3K27ac.metafile.bam"
+cmd="samtools merge H3K27ac.metafile.bam EGAD0000100267?_H3K27ac_50samples.metafile.bam && samtools sort -o H3K27ac.sorted.metafile.bam H3K27ac.metafile.bam"
 bsub -o samtools_merging_and_sorting_H3K27ac.out -R "rusage[mem=200]" -M 200000 -n 30 -R "span[hosts=1]" -J "samtools_merging_and_sorting_H3K27ac" " $cmd"
 echo "H3K27ac.metafile.bam created"
 
