f94a15f78d3be4943a464ab2f1f633afa6b09d27
Diana Avalos <diana.avalos.m@gmail.com>
2019-12-05 11:15:39 +0100

    not tested but goal to write in a histone loop for more modular code

--- diana_scripts/2_merge_metabamfiles_bjobs.sh
+++ diana_scripts/2_merge_metabamfiles_bjobs.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
-# merge metabamfiles with samtools
-cmd="samtools merge H3K4me1.metafile.bam EGAD0000100267?_H3K4me1_50samples.metafile.bam && samtools sort -o H3K4me1.sorted.metafile.bam H3K4me1.metafile.bam"
-bsub -o samtools_merging_and_sorting_H3K4me1.out -R "rusage[mem=2000]" -M 2000000 -n 30 -R "span[hosts=1]" -J "samtools_merging_and_sorting_H3K4me1" " $cmd"
-echo "H3K4me1.metafile.bam created"
+declare -a Histone_Modifications=("H3K4me1" "H3K27ac")
 
 
-cmd="samtools merge H3K27ac.metafile.bam EGAD0000100267?_H3K27ac_50samples.metafile.bam && samtools sort -o H3K27ac.sorted.metafile.bam H3K27ac.metafile.bam"
-bsub -o samtools_merging_and_sorting_H3K27ac.out -R "rusage[mem=200]" -M 200000 -n 30 -R "span[hosts=1]" -J "samtools_merging_and_sorting_H3K27ac" " $cmd"
-echo "H3K27ac.metafile.bam created"
+for histone in ${Histone_Modifications[@]}; do
+    FILENAME="samtools_merging_and_sorting_$histone"
+
+    # merge metabamfiles with samtools
+    cmd="samtools merge ${histone}.metafile.bam EGAD0000100267?_${histone}_50samples.metafile.bam && samtools sort -o ${histone}.sorted.metafile.bam ${histone}.metafile.bam"
+    
+    job="bsub -o ${FILENAME}.out -R \"rusage[mem=2000]\" -M 2000000 -n 30 -R \"span[hosts=1]\" -J \"$FILENAME\" \"$cmd\""
+    
+    echo "$job"
+    eval $job
+done
 
 
 
