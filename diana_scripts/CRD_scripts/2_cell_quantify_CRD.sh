148ac88f359a511abb7fe7caf7e995a1d79c34d4
dianamatata <diana.avalos.m@gmail.com>
2020-03-03 11:58:27 +0100

    changes

--- diana_scripts/CRD_scripts/2_cell_quantify_CRD.sh
+++ diana_scripts/CRD_scripts/2_cell_quantify_CRD.sh
@@ -6,23 +6,20 @@ DATADIR=/home/users/a/avalosma/scratch/1_CRD
 OUT_FOLDER=/home/users/a/avalosma/scratch/2_CRD
 mkdir -p $OUT_FOLDER $OUT_FOLDER/OUT
 
-#Loop on all cell types:
-# declare -a Cell_Types=("EGAD00001002673" "EGAD00001002672" "EGAD00001002670")
+for cell_type in 'EGAD00001002670'  'EGAD00001002672' 'EGAD00001002673' ; do
+	LI=$DATADIR_0/${cell_type}_merged_residuals.bed.gz
+        LM=$DATADIR/${cell_type}_ALL.modules.MOD1.NRE2.txt.gz
+	for c in $(seq 1 222); do
+		echo "$cell_type $c"
+		LT=$DATADIR/$cell_type\.chr$c\.module.txt
+		LO=$OUT_FOLDER/$cell_type\.chr$c\
 
-cell_type="EGAD00001002670"
+		cmd="$CLOMICs quantify --bed $LI --region $c --tree $LT $LM --out $LO\.pc1.txt.gz --pca 1 --normal"
+		cmd2="$CLOMICs quantify --bed $LI --region $c --tree $LT $LM --out $LO\.mean.txt.gz --mean --normal"
+		cmd3="$CLOMICs quantify --bed $LI --region $c --tree $LT $LM --out $LO\.loom.txt.gz --loo 0 --normal"
 
-for c in $(seq 1 2); do
-    echo "$c"
-    LI=$DATADIR_0/${cell_type}_merged_residuals.bed.gz
-    LM=$DATAFOLDER/${cell_type}_ALL.modules.MOD1.NRE2.txt.gz
-    LT=$DATADIR/$cell_type\.chr$c\.module.txt.gz
-    LO=$OUT_FOLDER/$cell_type\.chr$c\.pc1
-
-    cmd="$CLOMICS quantify --bed $LI --region $c --tree $LT $LM --out $LO\.pc1.txt.gz --pca 1 --normal"
-    cmd2="$CLOMICS quantify --bed $LI --region $c --tree $LT $LM --out $LO\.mean.txt.gz --mean --normal"
-    cmd3="$CLOMICS quantify --bed $LI --region $c --tree $LT $LM --out $LO\.loom.txt.gz --loo 0 --normal"
-    
-    echo "$cmd"
-    JOB=$OUT_FOLDER/OUT/${cell_type}_${c}_quantify
-    sbatch -J $JOB\.job --partition=mono-EL7 --time=00:08:00 -o $JOB\.out -e $JOB\.err --wrap="$cmd && $cmd2 && $cmd3" 
+		#echo "$cmd"
+		JOB=$OUT_FOLDER/OUT/${cell_type}_chr${c}_quantify
+		sbatch -J $JOB\.job --partition=mono-EL7 --time=00:01:00 -o $JOB\.out -e $JOB\.err --wrap="$cmd && $cmd2 && $cmd3" 
+	done
 done
