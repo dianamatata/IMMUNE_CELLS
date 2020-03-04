8539aa7bdcbc4ace1c51b6bd813849e79665e3f1
dianamatata <diana.avalos.m@gmail.com>
2020-03-04 15:30:10 +0100

    adding first draft for QTLtools quant on RNA data

---
+++ diana_scripts/CRD_scripts/4a_qtltools_quant_rna_data.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+QTLTOOLS=/home/users/a/avalosma/bin/qtltools
+IN=/home/users/a/avalosma/scratch/Blueprint/ref_files/gencode.v15.annotation.gtf.gzi
+IN_BAM=i/home/users/a/avalosma/scratch/Blueprint/RNA_seq
+OUTFOLDER=/home/users/a/avalosma/scratch/RNA_seq
+mkdir -p $OUTPUT_FOLDER $OUTPUT_FOLDER/OUT
+
+#for cell_type in 'EGAD00001002675'  'EGAD00001002674' 'EGAD00001002671' ; do 
+cell_type='EGAD00001002675'
+
+cmd="$QTLTOOLS quan --gtf $IN --bam $IN_BAM/$cell_type/*.bam --out qtltools_quantification_$cell_type --filter-mapping-quality 255 --rpkm "
+
+JOB=qtltools_quant_rpkm
+
+sbatch -J $JOB\.job --partition=mono-EL7 --time=20:00:00 -o $OUT_FOLDER/OUT/$JOB.out -e $OUT_FOLDER/OUT/$JOB.err --wrap="$cmd"
