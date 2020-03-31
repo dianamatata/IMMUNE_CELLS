9fd2a2397dd6ddc598600c31dbce774a70bcd67f
dianamatata <diana.avalos.m@gmail.com>
2020-03-31 18:26:41 +0200

    adjusted job time

--- diana_scripts/RNA_1b_quan_per_sample.sh
+++ diana_scripts/RNA_1b_quan_per_sample.sh
@@ -21,6 +21,6 @@ for cell_type in 'EGAD00001002671'  'EGAD00001002674' 'EGAD00001002675' ; do
 		out_name="$(echo $file | cut -d '/' -f10 | cut -c1-8)"
 		cmd="QTLtools quan --gtf $REF_FOLDER/gencode.v15.annotation.gtf.gz --bam $file --out $OUT_FOLDER/${cell_type}_qltools_quan_$out_name --filter-mapping-quality 255 --rpkm"
 	        #sbatch -J ${cell_type}_RNAquan.job --partition=mono-EL7 --time=03:00:00 -o $OUTPUT_FOLDER/OUT/${cell_type}_RNAquan_$out_name.out -e $OUTPUT_FOLDER/OUT/${cell_type}_RNAquan_$out_name.err --wrap="$cmd"
-		sbatch -J ${cell_type}_RNAquan.job --partition=mono-EL7 --time=10:00:00 --wrap="$cmd"
+		sbatch -J ${cell_type}_RNAquan.job --partition=mono-EL7 --time=03:00:00 --wrap="$cmd"
 	done
 done
