d26ffcbbd1cade67ecc9162cb65575b58d845284
dianamatata <diana.avalos.m@gmail.com>
2020-03-31 18:26:01 +0200

    adjusted job time

--- diana_scripts/RNA_0_create_bam_indices.sh
+++ diana_scripts/RNA_0_create_bam_indices.sh
@@ -6,6 +6,6 @@ DATA_FOLDER=/home/users/a/avalosma/scratch/Blueprint/RNA_seq
 for cell_type in 'EGAD00001002671'  'EGAD00001002674' 'EGAD00001002675' ; do
         for file in $DATA_FOLDER/${cell_type}/*.bam ; do
 		cmd="samtools index -b $file"
-		sbatch -J bai_$file.job --partition=mono-EL7 --time=01:00:00 --wrap="$cmd"
+		sbatch -J bai_$file.job --partition=mono-EL7 --time=03:00:00 --wrap="$cmd"
 	done
 done
