8d603c1689f39ed26c7800b12bc0e1607a57c173
dianamatata <diana.avalos.m@gmail.com>
2020-05-13 14:15:46 +0200

    correct path

--- diana_scripts/pathways/4_annotate.sh
+++ diana_scripts/pathways/4_annotate.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-$inFolder='trans_hubs_genes'
-$refGen='hg37'
+inFolder='/home/users/a/avalosma/IMMUNE_CELLS/diana_scripts/pathways/trans_hubs_genes'
+refGen='hg19'
+outFile='/home/users/a/avalosma/IMMUNE_CELLS/diana_scripts/pathways/annotations/annotate'
 for file in "$inFolder"/*; do
         printf $file
 	file_nbr=$(echo $file | awk -F '[_.]'  '{print $4"_"$5}')
 	printf $file_nbr
-	annotatePeaks.pl $file $refGen > ${outFile}_$file_nbr
+	annotatePeaks.pl $file $refGen > ${outFile}_${refGen}_${file_nbr}.txt
 done
 
