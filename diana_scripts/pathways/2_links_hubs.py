e5a01aa52e97398ef9a5f20efea068bf6a91f059
Diana Avalos <diana.avalos.m@gmail.com>
2020-05-18 16:23:35 +0200

    gets all the crd clusters

--- diana_scripts/pathways/2_links_hubs.py
+++ diana_scripts/pathways/2_links_hubs.py
@@ -4,27 +4,33 @@ import sys
 import re
 import csv
 
-inFolder='/home/users/a/avalosma/IMMUNE_CELLS/diana_scripts/pathways/trans_hubs_crds2'
-inFolder='/Users/AvalosDiana/GitHub/IMMUNE_CELLS/diana_scripts/pathways/trans_hubs_crds2'
+CRED = '\033[91m'
+CEND = '\033[0m'
+
+inFolder = '/home/users/a/avalosma/IMMUNE_CELLS/diana_scripts/pathways/trans_hubs_crds2'
+inFolder = '/Users/AvalosDiana/GitHub/IMMUNE_CELLS/diana_scripts/pathways/trans_hubs_crds2'
+
 
 def find_indices_of_occurence(dataframe, crds):
     """
     find in dataframe the indexes in which the crds selected appear
     :param dataframe:
     :param crds:
+    dataframe=links
+    crds=crd_list
     """
-    list = dataframe.index[dataframe.apply(lambda x: x.astype(str).str.contains(crds[0]).any(), axis=1)].tolist()
+    list_oc = dataframe.index[dataframe.apply(lambda x: x.astype(str).str.contains(crds[0]).any(), axis=1)].tolist()
     for crd in crds[1:]:
-        list.extend(dataframe.index[dataframe.apply(lambda x: x.astype(str).str.contains(crd).any(), axis=1)])
-    return list
+        list_oc.extend(dataframe.index[dataframe.apply(lambda x: x.astype(str).str.contains(crd).any(), axis=1)])
+    list_oc = list(dict.fromkeys(list_oc))
+    return list_oc
 
 
-def update_dict_node(dict_node, idx_crd):
+def update_dict_node(dict_node, idx_crd, crd_list):
     # add the crds to the dictionary, update the count of occurrence
     # if it is a new crd, add it to the new_crd_list
     new_crd_list = []
     for idx in idx_crd:
-        # print (links.iloc[idx]) #true index and count of lines, Name: 26, idx=19, reindex?
         crds = [links.iloc[idx]['id1'], links.iloc[idx]['id2']]
         for crd in crds:
             if crd in dict_node.keys():
@@ -32,6 +38,7 @@ def update_dict_node(dict_node, idx_crd):
             else:
                 dict_node[crd] = 1
                 new_crd_list.append(crd)
+    new_crd_list = [x for x in new_crd_list if x not in crd_list]
     return dict_node, new_crd_list
 
 
@@ -46,31 +53,55 @@ def write_to_file(dataframe, outputfile):
         w.writerow([key, val])
 
 
+def update_links(links, new_links):
+    links = new_links.reset_index()
+    links = links.drop(['index'], axis=1)
+    return links
+
+
 for cell in ('70', '72', '73'):
-    links_path = "%s/LINKS_%s.txt" % (inFolder,cell)
+    links_path = "%s/LINKS_%s.txt" % (inFolder, cell)
     print(links_path)
     links = pd.read_csv(links_path, sep=' ')
+    links = links.reset_index()
+    links = links.drop(['index'], axis=1)
+    links = links.head(n=500)  # temporary
     node = 0
+
     while len(links) > 1:
-        # for each node
-        for index, row in links.head(n=1).iterrows():
-            print(index)
-            node += 1
-            dict_node = {row['id1']: 1, row['id2']: 1}
-            # look for indices of each appearance of the dict entries
-            crd_list = [row['id1'], row['id2']]
-            while len(crd_list) > 0:
-                idx_crd = find_indices_of_occurence(links, crd_list)
-                # add them to the dict (+ new crd list) or increase their count
-                dict_node, new_crd_list = update_dict_node(dict_node, idx_crd)
-                # pop rows from dataframe
-                new_links = links.drop(links.index[idx_crd])
-                # check new crds, loop till no more new crds
-                crd_list = new_crd_list
-                links = new_links.reset_index()
-                links = links.drop(['index'], axis=1)
-            # add to structure holding all the dictionaries
+        index = 0
+        node += 1
+        dict_node = {}
+        crd_list = [links.iloc[index]['id1'], links.iloc[index]['id2']]
+        idx_crd = find_indices_of_occurence(links, crd_list)  # look for indices of each appearance of the dict entries
+
+        if len(idx_crd) > 0:
+
+            while len(idx_crd) > 0:  # weird statement
+                dict_node, crd_list = update_dict_node(dict_node, idx_crd, crd_list)  # add them to the dict
+                print('nbr idx: %d, new crds: %d, length links= %d ' % (len(idx_crd), len(crd_list), len(links)))
+                temp_links = links
+                new_links = links.drop(links.index[idx_crd])  # pop rows from dataframe
+                links = update_links(links, new_links)
+                if (len(crd_list) > 0):
+                    idx_crd = find_indices_of_occurence(links, crd_list)
+                else:
+                    idx_crd = []
+                    print('  no new crd')
+
+            # add to structure holding all the dictionaries. weird i reinitiqte it all the time
             dict_sorted = {k: v for k, v in sorted(dict_node.items(), key=lambda item: item[1], reverse=True)}
-            print(len(dict_sorted))
-            dict_sorted.to_csv( "%s/dict_%s_%d.csv" % (inFolder,cell, node),
-                                sep="\t")
+            file = "%s/dict_%s_%d.csv" % (inFolder, cell, node)
+            print(CRED + "links=%d" % len(links) + CEND)
+            print(CRED + "dict_%s_%d.csv" % (cell, node) + CEND)
+            with open(file, 'wb') as f:
+                w = csv.writer(f)
+                w.writerows(dict_sorted.items())
+
+        else:
+            print('   else loop')
+            links = links.drop(links.index[0])
+            links = new_links.reset_index()
+            links = links.drop(['index'], axis=1)
+            print('no clusters, len links %d' % len(links))
+            idx_crd = find_indices_of_occurence(links, crd_list)
