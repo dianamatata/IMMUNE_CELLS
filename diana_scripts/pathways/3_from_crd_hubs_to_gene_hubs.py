5cbf48fc83484c1cd147b12db822394b854ee268
dianaavalos <diana.avalos.m@gmail.com>
2020-06-02 10:08:42 +0200

    update gene extraction from TRHs computed with igraph algo

--- diana_scripts/pathways/3_from_crd_hubs_to_gene_hubs.py
+++ diana_scripts/pathways/3_from_crd_hubs_to_gene_hubs.py
@@ -14,31 +14,29 @@ def write_to_file(dataframe, outputfile):
         w.writerow([key, val])
 
 
-path = '/Users/AvalosDiana/GitHub/IMMUNE_CELLS/diana_scripts/pathways'
+path = '/Users/dianaavalos/Programming/IMMUNE_CELLS/diana_scripts/pathways'
+path2='/Users/dianaavalos/Programming/IMMUNE_CELLS/diana_scripts/trans'
+
+# with opmmunity file from igraph community algo
 
 for cell in ('70', '72', '73'):
-    CRD_genes_file = "%s/cisCRD-gene_associations/mapping_gene_CRD_mean_ALL_%s.txt" % (path, cell)
-    CRD_genes_list = pd.read_csv(CRD_genes_file, header=None, sep=' ')
-    CRD_cluster_file = '%s/trans_hubs_crds/dict_%s_*' % (path, cell)
-    crd_clusters_list = glob.glob(CRD_cluster_file)
+    CRD_genes_list = pd.read_csv("%s/cisCRD-gene_associations/mapping_gene_CRD_mean_ALL_%s.txt" % (path, cell), header=None, sep=' ')
+    CRD_cluster_file = pd.read_csv('%s/TRH_%s.txt' % (path2, cell), header=0, sep='\t')
     CRD_genes_list.columns = ["phenotype_ID", "phenotype_ID_chr", "phenotype_ID_start", "phenotype_ID_end",
                               "phenotype_ID_strand", "nb_variants", "distance", "CRD_ID", "CRD_ID_chr", "CRD_ID_start",
                               "CRD_ID_end", "nominal_pval", "slope", "top_variant"]
-    # for Homer
-    CRD_genes_list['phenotype_ID_chr'] = CRD_genes_list['phenotype_ID_chr'].apply(lambda x: 'chr' + str(x))
-
     # loop on clusters
-    for cluster_i_file in crd_clusters_list:
-        print(cluster_i_file)
-        crds_in_cluster = pd.read_csv(cluster_i_file, sep=',', header=None, usecols=[0])[0]
+    for cluster in range(1, max(CRD_cluster_file['communities.membership'])+1):
+        CRDS_in_TRH=CRD_cluster_file['communities.names'][CRD_cluster_file['communities.membership'] == cluster]
+
         genes = pd.DataFrame(columns=["phenotype_ID", "phenotype_ID_chr", "phenotype_ID_start", "phenotype_ID_end",
                                       "phenotype_ID_strand", "CRD_ID"])
-        print(list(crds_in_cluster))
-        for CRD in crds_in_cluster:
-            # CRD = '11_internal_10924'
+        for CRD in CRDS_in_TRH:
+            #print(CRD)  # CRD = '11_internal_10924'
             subdf = CRD_genes_list.loc[CRD_genes_list['CRD_ID'] == CRD].iloc[:, [0, 1, 2, 3, 4, 7]]
             genes = genes.append(subdf)
 
         if (len(genes) > 0):
-            outname = "%s/trans_hubs_genes/dict_%s_%s.csv" % (path, cell, cluster_i_file.split("_")[6].split('.')[0])
+            outname = "%s/trans_hubs_genes/genes_%s_cluster%s.csv" % (path, cell, cluster)
             genes.to_csv(outname, index=False, sep='\t')
+            genes.phenotype_ID.to_csv("%s/trans_hubs_genes/genesonly_%s_cluster%s.csv" % (path, cell, cluster), index=False, sep='\t')
