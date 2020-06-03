0c33e26b6e0ea7bd3aa31b1b2589161799b5f6b8
dianaavalos <diana.avalos.m@gmail.com>
2020-06-03 16:33:48 +0200

    added plot to see the nbr of CRDs and genes associated per TRHs

--- diana_scripts/trans/TRHindex_3cell_types.R
+++ diana_scripts/trans/TRHindex_3cell_types.R
@@ -13,7 +13,7 @@ DATA_70 = as.data.frame(data.table::fread("/Users/dianaavalos/Programming/THREE_
 DATA_72 = as.data.frame(data.table::fread("/Users/dianaavalos/Programming/THREE_CELL_TYPES__CLOMICS__EGAD00001002670_CLOMICS_v3.0__TRANS/EGAD00001002672_ALL.ALLchr.txt.gz", head=FALSE, stringsAsFactors=FALSE))
 DATA_73 = as.data.frame(data.table::fread("/Users/dianaavalos/Programming/THREE_CELL_TYPES__CLOMICS__EGAD00001002670_CLOMICS_v3.0__TRANS/EGAD00001002673_ALL.ALLchr.txt.gz", head=FALSE, stringsAsFactors=FALSE))
 
-DATA=DATA_70
+DATA=DATA_70 ######## cell type
 colnames(DATA) = c("idx1","chr1","start1","end1","id1","idx2","chr2","start2","end2","id2","corr","pval")
 Q = qvalue(DATA$pval)
 DATA$qval = Q$qvalues
@@ -29,11 +29,42 @@ communities  = fastgreedy.community(DATAnet.TRH) # https://www.rdocumentation.or
 N_TRH = max(communities$membership)
 N_TRH_TOSHOW = 50
 
+length(unique(communities$membership))
+length(communities$names)
 COM = data.frame(communities$names, communities$membership)
 COM = COM[order(communities$membership),]
 
 path='/Users/dianaavalos/Programming/IMMUNE_CELLS/diana_scripts/trans'
+######## cell type
 write.table(file=file.path(path,'TRH_70.txt'),COM,quote=F,row.names=F,sep='\t')
+
+### PLOT
+df = data.frame(ID=1:N_TRH,SIZE=rle(sort(communities$membership))$length,stringsAsFactors=F)
+df <-df[order(-df$SIZE),]
+df['GENE']=0
+
+# plot gene number and plot the nbr of CRDs and genes associated per TRH
+
+path2='/Users/dianaavalos/Programming/IMMUNE_CELLS/diana_scripts/pathways/trans_hubs_genes'
+for (i in 1:N_TRH) {
+  file=file.path(path2,sprintf("genesonly_70_cluster%s.txt", i) ) ######## cell type
+  if (file.exists(file)){
+    df$GENE[i]=length(read.table(file, header = FALSE, sep = "", dec = ".")$V1)
+    #sprintf("70_cluster%s.txt", i)
+    #print(length(read.table(file, header = FALSE, sep = "", dec = ".")$V1))
+  }
+}
+df <-df[order(-df$GENE),]
+df$ID2=0
+for (j in 1:N_TRH) {  df$ID2[j]=j}
+
+dff <- data.frame(head(df),100)
+
+dfff <- melt(df[c(2,3,4)], id.vars='ID2')
+p3<- ggplot(dfff, aes(ID2, value)) +   
+  geom_bar(aes(fill = variable), position = "dodge", stat="identity") + theme(axis.text.x = element_text(angle = 90, hjust = 1))  + scale_y_continuous(trans = 'log10')
++ theme_minimal() 
+
 
 
 ###### PLOT:
